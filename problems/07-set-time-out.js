b45525279a5630c3b85abc63db77e0639ef37d26
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2023-08-17 04:19:08 -0500

    set-time-out

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -18,7 +18,15 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 ***********************************************************************/
 
-// Your code here
+let funcTimer = function(time, func) {
+  globalThis.setTimeout(func, time);
+}
+
+function partyFunc () {
+  console.log("Party time!")
+}
+
+funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
